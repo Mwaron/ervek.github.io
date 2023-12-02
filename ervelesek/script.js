6ea0a43cfc7c11263c5b7d9630deb8ed9c805d1d
Mwaron <marton.aron08@gmail.com>
2023-12-02 11:43:14 +0100

    Végső program
    Befejeztem mindent, még a színeket/kinézetet valószínüleg átállítom, de elég jó minden működik.

---
+++ ervelesek/script.js
@@ -0,0 +1,15 @@
+function toggleText() {
+    var text1 = document.getElementById('text2');
+    var text2 = document.getElementById('text1');
+  
+    text1.style.display = 'none';
+    text2.style.display = 'block';
+  }
+  
+  function toggleTextReverse() {
+    var text1 = document.getElementById('text2');
+    var text2 = document.getElementById('text1');
+  
+    text1.style.display = 'block';
+    text2.style.display = 'none';
+  }
